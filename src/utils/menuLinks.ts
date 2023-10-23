e15f539b25b9d6e76dcce8eafb3217bd47a98f8e
MeLoBeats <lamelobeats@gmail.com>
2023-10-23 14:04:29 +0200

    feat: add header, navbar

---
+++ src/utils/menuLinks.ts
@@ -0,0 +1,20 @@
+const menuLinks = [
+    {
+        name: 'Accueil',
+        link: '/'
+    },
+    {
+        name: 'Nos projets',
+        link: '/nos-projets'
+    },
+    {
+        name: 'L\'équipage',
+        link: '/equipage'
+    },
+    {
+        name: 'Contact',
+        link: '/contact'
+    },
+]
+
+export default menuLinks
